6d4cb07b93b5c98816b6d26d32a0f8df8c596883
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-05-01 23:30:57 +1000

    test/TestPhilox, TestThreefry: modified tests to check all counter positions

--- test/TestPhilox.hs
+++ test/TestPhilox.hs
@@ -10,17 +10,26 @@ import System.Random.Random123.Types
 import System.Random.Random123.Philox
 
 
+-- Reference results were obtained using Random123 v1.07
+
+
 -- Checking reference results for Philox-2x32-10
 
 test_philox2x32 = results @?= reference where
     key = 123 :: Array1 Word32
     rounds = 10
-    (counters, reference) = unzip [
-        ((0, 0), (0xf844fda3, 0x8471a7bc)),
-        ((1, 0), (0x1c0088c6, 0xe4cea27e)),
-        ((2, 0), (0xc90d7a20, 0xa64efdcc)),
-        ((3, 0), (0x58e869ff, 0xcf77dad5)),
-        ((4, 0), (0x4aa25904, 0x49041bf9))]
+    counters = [
+        (0x6f55b92a, 0xab1c5ea9),
+        (0x2e24118f, 0xf96cd57b),
+        (0x35c2faac, 0x229e22ea),
+        (0x71d82da5, 0xe7758e07),
+        (0xefdadeee, 0x58849b8f)]
+    reference = [
+        (0xc8a74eda, 0x43748166),
+        (0x0f2f49b3, 0x5440de8c),
+        (0x9b0b15f0, 0x62ad33f0),
+        (0x8e0d363a, 0xec949c27),
+        (0xef1c72c0, 0xabcdf6f6)]
     results = map (philox2R rounds key) counters
 
 
@@ -29,12 +38,18 @@ test_philox2x32 = results @?= reference where
 test_philox2x64 = results @?= reference where
     key = (123, 456) :: Array2 Word32
     rounds = 10
-    (counters, reference) = unzip [
-        ((0, 0, 0, 0), (0xd9392b80, 0x0a6a66ce, 0x010bc7c6, 0xe935bf8e)),
-        ((1, 0, 0, 0), (0x4e76cd54, 0x532901b2, 0x1889a270, 0x62b708d1)),
-        ((2, 0, 0, 0), (0xed888015, 0x7ff88042, 0x6cdd14da, 0xbc9a0c2f)),
-        ((3, 0, 0, 0), (0xa7de2b2e, 0xba57e68a, 0xe556b0fc, 0xbb1d6d0e)),
-        ((4, 0, 0, 0), (0x74f6a1ed, 0xcf62b2b6, 0x75639da9, 0x75191bda))]
+    counters = [
+        (0x5bf34ff4, 0x98f9badb, 0x4f816cab, 0xe62bd835),
+        (0xdfe40202, 0xdf14b7de, 0x9f66a741, 0xcf4157f7),
+        (0x839ac18e, 0xa043ba06, 0x94fd2987, 0x7d176d4c),
+        (0xdbba4414, 0x4a0462e2, 0x74cf99d4, 0x68d78dad),
+        (0x694f3a06, 0xc113b26e, 0x6b7be5fe, 0x47e59594)]
+    reference = [
+        (0x01c5a89a, 0xc8783325, 0x6fcbdf46, 0x0e5e2c08),
+        (0xbdfdcee8, 0x2302ddcb, 0x0c6b097d, 0xfe5b1b6d),
+        (0x41852fa7, 0xca2e06e0, 0x421b06e6, 0x825db2b2),
+        (0x858a0b3f, 0x9c4ee5ae, 0x409bca71, 0x6174268f),
+        (0x1fb47ee3, 0x465c46e9, 0xac8a8a63, 0x770b5eb8)]
     results = map (philox4R rounds key) counters
 
 
@@ -43,12 +58,18 @@ test_philox2x64 = results @?= reference where
 test_philox4x32 = results @?= reference where
     key = 123 :: Word64
     rounds = 10
-    (counters, reference) = unzip [
-        ((0, 0), (0xb22df93e9c84bfc5, 0xe0ea6a0a15f83873)),
-        ((1, 0), (0x128cf138d8d9060c, 0x4f526c315be5297d)),
-        ((2, 0), (0xae5a8a95e1d0b401, 0x69e09437eba9102c)),
-        ((3, 0), (0xb91ca19300f0a886, 0x449e73bb22afd2b7)),
-        ((4, 0), (0xd592e15a3fc37223, 0xeac7eca148f7cc1e))]
+    counters = [
+        (0xf5bd87c1f90bf000, 0xfdcf5569188d8000),
+        (0x1fcbd563f0fc8e00, 0x9eb6d45bd5096000),
+        (0x5baaae0aecd1e000, 0x53d0329123492c00),
+        (0x62ef71add3320800, 0x625c1fc53fd21000),
+        (0x83d3483f4f369000, 0x395f94432bdf0200)]
+    reference = [
+        (0x70aa5fafe6b960f0, 0xf0296d49836ec74d),
+        (0x5e6a58a58da65da7, 0xd58ae0056c4f6eb0),
+        (0x79feffb4c50d7a7e, 0x586c57e69f636783),
+        (0x9f0ef9aa1c02cbb3, 0x64d05e4f231859ea),
+        (0x529c1fe09c72783b, 0x6a3b27e993d5fd7b)]
     results = map (philox2R rounds key) counters
 
 
@@ -57,12 +78,18 @@ test_philox4x32 = results @?= reference where
 test_philox4x64 = results @?= reference where
     key = (123, 456) :: Array2 Word64
     rounds = 10
-    (counters, reference) = unzip [
-        ((0, 0, 0, 0), (0x3cdcad1fb4763de7, 0xf94cc91d1fab146b, 0x3aa7490df501df51, 0x458f65a8c046a6fa)),
-        ((1, 0, 0, 0), (0x182a33ef112a55c6, 0x7fa21420170db5b7, 0x3d065f703e33bef6, 0x29ec19a7d6e63a9a)),
-        ((2, 0, 0, 0), (0x280c361dcea8055e, 0x0de096e158cf6aa5, 0x47958d4675ff3bfa, 0xdebe3ce8f8da523e)),
-        ((3, 0, 0, 0), (0xab85413121df79f0, 0x034ac8f5f81151ed, 0xf8689591c201eab8, 0xdaa456ec24eb937e)),
-        ((4, 0, 0, 0), (0x45fe5ec993667f29, 0x392a9d088bdaf5b0, 0xa09f0622332af717, 0xccee2dda90f6d9f7))]
+    counters = [
+        (0x1d1fc5123d698500, 0x6a0fd3ccbc9d3800, 0xe93a590b53026000, 0x3b339cb99a06fa00),
+        (0x22f53249be020e00, 0xcfbd27dc0b0dc000, 0xbe8468a6b6ac3000, 0x0428398af2cfcac0),
+        (0xd07b0391ea202000, 0xc6bb23b0fb74c800, 0x62720dec16217800, 0xdd5d41a2c6c29800),
+        (0x4f9ffd1a930a3800, 0x41ee8eae5f157c00, 0x1e7ac9fcae5af500, 0x9e618d09ef001000),
+        (0x9712f6164d422800, 0xc04f3f2e6d87a800, 0x7e2369ed83cdac00, 0xdb986d520bfcc000)]
+    reference = [
+        (0xcf1ee3ae885c3928, 0x90a39b52bb92f018, 0x1e2c1e36cdafc6d5, 0x2068f5a63acafdbb),
+        (0x28b86429c0cc8d2f, 0xedb2c9aeaa6ef776, 0x7615c3e5815ea96c, 0xdcf5e8849a76aec0),
+        (0x775904470661a226, 0xeceefd4563c20c51, 0x5d842c642c640a6b, 0xd17f2f87ad2fa74e),
+        (0x00c263e4bc04441f, 0xaecb35c835e03874, 0x46bb918e4a85adb7, 0x6c5fd577457f6315),
+        (0x61ca2527c8acdb96, 0x87c6dd6227eb5256, 0x6901e9c532adea59, 0x9bd765a53cca4d5f)]
     results = map (philox4R rounds key) counters
 
 
--- test/TestThreefry.hs
+++ test/TestThreefry.hs
@@ -10,17 +10,27 @@ import System.Random.Random123.Types
 import System.Random.Random123.Threefry
 
 
+-- Reference results were obtained using Random123 v1.07
+-- (with a minor bug fixed --- rotation constant order is broken in 20-th round)
+
+
 -- Checking reference results for Threefry-2x32-32
 
 test_threefry2x32 = results @?= reference where
     key = (123, 456) :: Array2 Word32
     rounds = 32
-    (counters, reference) = unzip [
-        ((0, 0), (0x44fa0a46, 0xee68d198)),
-        ((1, 0), (0x5d7c148b, 0x1b5642f9)),
-        ((2, 0), (0xda135215, 0x9ff4dc46)),
-        ((3, 0), (0xa659b4bf, 0x5cfa153b)),
-        ((4, 0), (0xa183bd9e, 0x06ce511e))]
+    counters = [
+        (0x058741f8, 0x346b43e7),
+        (0xcedc5891, 0x2c5c1d28),
+        (0x5a086b94, 0x1acbdc33),
+        (0xa76cc7bf, 0x857c8f0f),
+        (0xad8d9e57, 0x01b1d9fa)]
+    reference = [
+        (0x2d4c5ff6, 0x76375d6e),
+        (0x78b73f35, 0xea905a45),
+        (0xfde30fed, 0x9c104974),
+        (0x15861c3d, 0x293ba61f),
+        (0x5033c8d1, 0x7b92151a)]
     results = map (threefry2R rounds key) counters
 
 
@@ -29,12 +39,18 @@ test_threefry2x32 = results @?= reference where
 test_threefry2x64 = results @?= reference where
     key = (123, 456, 789, 101112) :: Array4 Word32
     rounds = 72
-    (counters, reference) = unzip [
-        ((0, 0, 0, 0), (0xc67a15ff, 0x1f294109, 0x18a392cd, 0x901ff30c)),
-        ((1, 0, 0, 0), (0x849a55f4, 0x23948454, 0x9cc01d9a, 0xf610ec3e)),
-        ((2, 0, 0, 0), (0x59ced9b5, 0x22d44d98, 0x9a2e6b4c, 0x8a6d428f)),
-        ((3, 0, 0, 0), (0x81b5a49e, 0x5a2aff79, 0x5da97cc5, 0x774929fd)),
-        ((4, 0, 0, 0), (0x543952ef, 0x9ac89c2d, 0x9f2a6a6a, 0x7dab5f5f))]
+    counters = [
+        (0x589c8db6, 0x19d662ad, 0x3e63b1e5, 0x25dad2c5),
+        (0x403512be, 0x370c9a86, 0x66d4c418, 0x92ed3769),
+        (0x1920eb77, 0x2d6340fe, 0xa2f618e3, 0xb8e4aacf),
+        (0x66005aa2, 0xf70ecfdf, 0xe38e29c5, 0xcbbd54d2),
+        (0x295e45f0, 0xa3d266cd, 0xb102152d, 0x2f57b507)]
+    reference = [
+        (0xcc07f462, 0x18cbe434, 0x97b800fe, 0xc515a3c1),
+        (0x67bf63ec, 0x08274d21, 0x18557796, 0x36706c15),
+        (0x428a06be, 0xbf1caad7, 0x25e51e44, 0x89e4bd74),
+        (0xbfff9163, 0x4106e304, 0x03157e56, 0xebfbd11c),
+        (0x5a1f4a9c, 0x1725d096, 0x2b561b17, 0xeb9b4807)]
     results = map (threefry4R rounds key) counters
 
 
@@ -43,12 +59,18 @@ test_threefry2x64 = results @?= reference where
 test_threefry4x32 = results @?= reference where
     key = (123, 456) :: Array2 Word64
     rounds = 32
-    (counters, reference) = unzip [
-        ((0, 0), (0x2f94adcdbf5e99b0, 0x8ceec9683bbd82cc)),
-        ((1, 0), (0xe27c9db97d68bfeb, 0x0509d05b85fe1752)),
-        ((2, 0), (0x790eca0901fd3d96, 0x8a2bdd2096be6ff7)),
-        ((3, 0), (0x0e08a49c8f398236, 0xe8e60680e60e8c27)),
-        ((4, 0), (0x31448e1bce8f6d81, 0xba45258cb93f2aa5))]
+    counters = [
+        (0x097a4f5083c77380, 0x51794d6039c75400),
+        (0x65918ba6ccfe6400, 0x3d91c69276916c00),
+        (0xdfe3269c92f1f800, 0xe6a0ab67f2207800),
+        (0x6240b418d521f000, 0x40299ed82854f400),
+        (0x9acc98590f691000, 0x89df228bd6bae800)]
+    reference = [
+        (0xa6102e76d20f3382, 0x44dbf36431f9e9ee),
+        (0x0da48a45f9295d0e, 0x95d3b41c67e0f4c2),
+        (0x050a8dfccae89041, 0x76c474fad535292d),
+        (0xea84579be191a8fc, 0x3236c9527cba73e9),
+        (0x31f9c11860715320, 0x7ba946b5076efd08)]
     results = map (threefry2R rounds key) counters
 
 
@@ -57,12 +79,18 @@ test_threefry4x32 = results @?= reference where
 test_threefry4x64 = results @?= reference where
     key = (123, 456, 789, 101112) :: Array4 Word64
     rounds = 72
-    (counters, reference) = unzip [
-        ((0, 0, 0, 0), (0x92c90c3543470fd7, 0xcbbdc0c2672a29d3, 0x4c72604bb3049e52, 0x2fc833631da64583)),
-        ((1, 0, 0, 0), (0x9ca0c9ec4e63fd6a, 0x1f9c5471ba5ddcfc, 0x7497d8d9f036a812, 0x90dfcf3afd0bedf5)),
-        ((2, 0, 0, 0), (0xb868ddbd7959bcc1, 0xf72e76dd0520087a, 0x284b40cc68956c0e, 0x84f883b34173599d)),
-        ((3, 0, 0, 0), (0xfbaa3c5e47f504e7, 0x6f2649add2a7711b, 0x6a5cb2e2376756e1, 0xbd5e8aa4af1258a6)),
-        ((4, 0, 0, 0), (0xe984b393bc787737, 0x884b7393f32ae770, 0x3185c98d31175081, 0xb93d4c8d1a0d0b7f))]
+    counters = [
+        (0x916c0b160f927800, 0x52eb051cf7d14400, 0xbb685306834af800, 0x472bc9ebd7c67000),
+        (0x61b7386b8fea1800, 0x8e3c63aaaf6f0000, 0x32a99c88983d4400, 0x0e6631cb0b255a00),
+        (0x5ac82c4303a38400, 0xe3b2d6d630f00000, 0xb0fd6567c0918800, 0x103c4caced3ad200),
+        (0x8df94ae6b44d9000, 0x6a20b71e020c1800, 0x7a7554df0c355000, 0x0ab535e581250880),
+        (0x2f8ec42369683200, 0xda1197831643b000, 0xe1e589e32f32c800, 0x0b7a9b0053df1480)]
+    reference = [
+        (0x8d72c51e3e02c62c, 0xf343d5bdd4428602, 0xc3769f920f6bb846, 0x68f81b2a1276a4e1),
+        (0x333f98bad6286630, 0xbd1330b5f841bc3a, 0x95f3aab12571ae68, 0xbc2f487dd4b9d63c),
+        (0xebec496e8c1198ab, 0xdd7ea52e93dbf166, 0x6894711ca28c7c12, 0x5817ba9c9f36f1bc),
+        (0x892f196d93952843, 0x4341d023d4fe64f3, 0x72bd9fba2c6ed9c3, 0x5f7affd1dd0b449f),
+        (0xc91f23d22b34be06, 0x8ddbf89963b13e67, 0x230bb28bfb3f4b3c, 0xefaa712baaa3dafb)]
     results = map (threefry4R rounds key) counters
 
 
