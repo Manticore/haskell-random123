708c36c49acc4770e2aa3dda30b32cf8321c63b7
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-05-01 23:06:31 +1000

    Added Types module (only synonyms at the moment)

---
+++ System/Random/Random123/Types.hs
@@ -0,0 +1,6 @@
+-- | Type synonyms for use in function and instance declarations.
+module System.Random.Random123.Types where
+
+type Array1 a = a
+type Array2 a = (a, a)
+type Array4 a = (a, a, a, a)
