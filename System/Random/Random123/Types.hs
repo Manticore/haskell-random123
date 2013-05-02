671017b18a1b56368ea2342d366bbbabb69c9986
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-05-02 17:19:18 +1000

    Types: added typeclasses for use in RandomGen

--- System/Random/Random123/Types.hs
+++ System/Random/Random123/Types.hs
@@ -1,6 +1,180 @@
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, UndecidableInstances #-}
+
 -- | Type synonyms for use in function and instance declarations.
 module System.Random.Random123.Types where
+
+import Data.Bits
+import Data.Word
 
 
 type Array2 a = (a, a)
 type Array4 a = (a, a, a, a)
+
+
+-- | Class of integers with more bits than in simple types yet having fixed limited size
+-- (unlike the built-in 'Integer').
+class LimitedInteger a where
+    liFromInteger :: Integer -> a
+    liToInteger :: a -> Integer
+    liBitSize :: a -> Int
+
+-- Technically, Word32 and Word64 instances are identical,
+-- but I couldn't persuade GHC to compile them in generalized form
+-- (like "instance (Num a, Bits a, Integral a) => LimitedInteger (Array2 a)").
+
+instance LimitedInteger Word32 where
+    liFromInteger = fromInteger
+    liToInteger = toInteger
+    liBitSize = bitSize
+
+instance LimitedInteger (Array2 Word32) where
+    liFromInteger i = (x0, x1) where
+        bits = bitSize (undefined :: Word32)
+        x0 = fromInteger (i `shiftR` bits)
+        x1 = fromInteger i
+    liToInteger (x0, x1) = x0' + x1' where
+        bits = bitSize (undefined :: Word32)
+        x0' = toInteger x0 `shiftL` bits
+        x1' = toInteger x1
+    liBitSize _ = bitSize (undefined :: Word32) * 2
+
+instance LimitedInteger (Array4 Word32) where
+    liFromInteger i = (x0, x1, x2, x3) where
+        bits = bitSize (undefined :: Word32)
+        x0 = fromInteger (i `shiftR` (bits * 3))
+        x1 = fromInteger (i `shiftR` (bits * 2))
+        x2 = fromInteger (i `shiftR` bits)
+        x3 = fromInteger i
+    liToInteger (x0, x1, x2, x3) = x0' + x1' + x2' + x3' where
+        bits = bitSize (undefined :: Word32)
+        x0' = toInteger x0 `shiftL` (bits * 3)
+        x1' = toInteger x1 `shiftL` (bits * 2)
+        x2' = toInteger x2 `shiftL` bits
+        x3' = toInteger x3
+    liBitSize _ = bitSize (undefined :: Word32) * 4
+
+
+instance LimitedInteger Word64 where
+    liFromInteger = fromInteger
+    liToInteger = toInteger
+    liBitSize = bitSize
+
+instance LimitedInteger (Array2 Word64) where
+    liFromInteger i = (x0, x1) where
+        bits = bitSize (undefined :: Word64)
+        x0 = fromInteger (i `shiftR` bits)
+        x1 = fromInteger i
+    liToInteger (x0, x1) = x0' + x1' where
+        bits = bitSize (undefined :: Word64)
+        x0' = toInteger x0 `shiftL` bits
+        x1' = toInteger x1
+    liBitSize _ = bitSize (undefined :: Word64) * 2
+
+instance LimitedInteger (Array4 Word64) where
+    liFromInteger i = (x0, x1, x2, x3) where
+        bits = bitSize (undefined :: Word64)
+        x0 = fromInteger (i `shiftR` (bits * 3))
+        x1 = fromInteger (i `shiftR` (bits * 2))
+        x2 = fromInteger (i `shiftR` bits)
+        x3 = fromInteger i
+    liToInteger (x0, x1, x2, x3) = x0' + x1' + x2' + x3' where
+        bits = bitSize (undefined :: Word64)
+        x0' = toInteger x0 `shiftL` (bits * 3)
+        x1' = toInteger x1 `shiftL` (bits * 2)
+        x2' = toInteger x2 `shiftL` bits
+        x3' = toInteger x3
+    liBitSize _ = bitSize (undefined :: Word64) * 4
+
+
+-- | Class of CBRNG counters.
+class LimitedInteger a => Counter a where
+    skip :: Integer -> a -> a
+    skip i x = liFromInteger (liToInteger x + i)
+    increment :: a -> a
+    increment = skip 1
+
+
+instance (LimitedInteger (Array2 a), Bits a, Ord a, Num a) => Counter (Array2 a) where
+    increment (c0, c1)
+        | c1 < maxnum = (c0, c1 + 1)
+        | otherwise = (c0 + 1, 0)
+        where
+            maxnum = 2 ^ (bitSize c0) - 1
+
+instance (LimitedInteger (Array4 a), Bits a, Ord a, Num a) => Counter (Array4 a) where
+    increment (c0, c1, c2, c3)
+        | c3 < maxnum = (c0, c1, c2, c3 + 1)
+        | c2 < maxnum = (c0, c1, c2 + 1, 0)
+        | c1 < maxnum = (c0, c1 + 1, 0, 0)
+        | otherwise = (c0 + 1, 0, 0, 0)
+        where
+            maxnum = 2 ^ (bitSize c0) - 1
+
+
+-- | Class of objects allowing the extraction of 32-bit words from a given position.
+class Word32Array a where
+    getWord32 :: Int -> a -> Word32
+    numWords32 :: a -> Int
+
+instance Word32Array (Array2 Word32) where
+    getWord32 0 (x0, x1) = x0
+    getWord32 1 (x0, x1) = x1
+    numWords32 _ = 2
+
+instance Word32Array (Array4 Word32) where
+    getWord32 0 (x0, x1, x2, x3) = x0
+    getWord32 1 (x0, x1, x2, x3) = x1
+    getWord32 2 (x0, x1, x2, x3) = x2
+    getWord32 3 (x0, x1, x2, x3) = x3
+    numWords32 _ = 4
+
+instance Word32Array (Array2 Word64) where
+    getWord32 0 (x0, x1) = fromIntegral (x0 `shiftR` 32)
+    getWord32 1 (x0, x1) = fromIntegral x0
+    getWord32 2 (x0, x1) = fromIntegral (x1 `shiftR` 32)
+    getWord32 3 (x0, x1) = fromIntegral x1
+    numWords32 _ = 4
+
+instance Word32Array (Array4 Word64) where
+    getWord32 0 (x0, x1, x2, x3) = fromIntegral (x0 `shiftR` 32)
+    getWord32 1 (x0, x1, x2, x3) = fromIntegral x0
+    getWord32 2 (x0, x1, x2, x3) = fromIntegral (x1 `shiftR` 32)
+    getWord32 3 (x0, x1, x2, x3) = fromIntegral x1
+    getWord32 4 (x0, x1, x2, x3) = fromIntegral (x2 `shiftR` 32)
+    getWord32 5 (x0, x1, x2, x3) = fromIntegral x2
+    getWord32 6 (x0, x1, x2, x3) = fromIntegral (x3 `shiftR` 32)
+    getWord32 7 (x0, x1, x2, x3) = fromIntegral x3
+    numWords32 _ = 8
+
+
+-- | Class of objects allowing the extraction of 64-bit words from a given position.
+class Word64Array a where
+    getWord64 :: Int -> a -> Word64
+    numWords64 :: a -> Int
+
+instance Word64Array (Array2 Word32) where
+    getWord64 0 (x0, x1) = hi `shiftL` 32 + lo where
+        lo = fromIntegral x1 :: Word64
+        hi = fromIntegral x0 :: Word64
+    numWords64 _ = 1
+
+instance Word64Array (Array4 Word32) where
+    getWord64 0 (x0, x1, x2, x3) = hi `shiftL` 32 + lo where
+        lo = fromIntegral x1 :: Word64
+        hi = fromIntegral x0 :: Word64
+    getWord64 1 (x0, x1, x2, x3) = hi `shiftL` 32 + lo where
+        lo = fromIntegral x2 :: Word64
+        hi = fromIntegral x3 :: Word64
+    numWords64 _ = 2
+
+instance Word64Array (Array2 Word64) where
+    getWord64 0 (x0, x1) = x0
+    getWord64 1 (x0, x1) = x1
+    numWords64 _ = 2
+
+instance Word64Array (Array4 Word64) where
+    getWord64 0 (x0, x1, x2, x3) = x0
+    getWord64 1 (x0, x1, x2, x3) = x1
+    getWord64 2 (x0, x1, x2, x3) = x2
+    getWord64 3 (x0, x1, x2, x3) = x3
+    numWords64 _ = 4
