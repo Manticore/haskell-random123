3a64faa34b3cd24c8f98ad92423f4e038d8c0cea
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-05-02 17:36:42 +1000

    TestTypes: added tests for skip

--- test/TestTypes.hs
+++ test/TestTypes.hs
@@ -53,21 +53,44 @@ test_li4x64_bitsize li = (liToInteger li) < 2 ^ (liBitSize li) where
     types = (li :: Array4 Word64)
 
 
-test_types = testGroup "LimitedInteger" [
-    testGroup "liFromInteger . liToInteger == id" [
-        testProperty "1x32" test_li1x32_bijection,
-        testProperty "2x32" test_li2x32_bijection,
-        testProperty "4x32" test_li4x32_bijection,
-        testProperty "1x64" test_li1x64_bijection,
-        testProperty "2x64" test_li2x64_bijection,
-        testProperty "4x64" test_li4x64_bijection
+test_ctr2x32_skip ctr i = liFromInteger (liToInteger ctr + i) == skip i ctr where
+    types = (ctr :: Array2 Word32, i :: Integer)
+
+test_ctr4x32_skip ctr i = liFromInteger (liToInteger ctr + i) == skip i ctr where
+    types = (ctr :: Array4 Word32, i :: Integer)
+
+test_ctr2x64_skip ctr i = liFromInteger (liToInteger ctr + i) == skip i ctr where
+    types = (ctr :: Array2 Word64, i :: Integer)
+
+test_ctr4x64_skip ctr i = liFromInteger (liToInteger ctr + i) == skip i ctr where
+    types = (ctr :: Array4 Word64, i :: Integer)
+
+
+test_types = testGroup "Typeclasses" [
+    testGroup "LimitedInteger" [
+        testGroup "liFromInteger . liToInteger == id" [
+            testProperty "1x32" test_li1x32_bijection,
+            testProperty "2x32" test_li2x32_bijection,
+            testProperty "4x32" test_li4x32_bijection,
+            testProperty "1x64" test_li1x64_bijection,
+            testProperty "2x64" test_li2x64_bijection,
+            testProperty "4x64" test_li4x64_bijection
+            ],
+        testGroup "liToInteger < 2 ^ liBitSize" [
+            testProperty "1x32" test_li1x32_bitsize,
+            testProperty "2x32" test_li2x32_bitsize,
+            testProperty "4x32" test_li4x32_bitsize,
+            testProperty "1x64" test_li1x64_bitsize,
+            testProperty "2x64" test_li2x64_bitsize,
+            testProperty "4x64" test_li4x64_bitsize
+            ]
         ],
-    testGroup "liToInteger < 2 ^ liBitSize" [
-        testProperty "1x32" test_li1x32_bitsize,
-        testProperty "2x32" test_li2x32_bitsize,
-        testProperty "4x32" test_li4x32_bitsize,
-        testProperty "1x64" test_li1x64_bitsize,
-        testProperty "2x64" test_li2x64_bitsize,
-        testProperty "4x64" test_li4x64_bitsize
+    testGroup "Counter" [
+        testGroup "skip behaves like the default implementation" [
+            testProperty "2x32" test_ctr2x32_skip,
+            testProperty "4x32" test_ctr4x32_skip,
+            testProperty "2x64" test_ctr2x64_skip,
+            testProperty "4x64" test_ctr4x64_skip
+            ]
         ]
     ]
