1b2308c8a2425f79df714d00ba03b6b91b6d95c5
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-05-02 17:19:44 +1000

    Added a submodule for RandomGen instance

--- System/Random/Random123.hs
+++ System/Random/Random123.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE FlexibleInstances #-}
 
-module System.Random.Random123 where
+module System.Random.Random123 (
+    philox2,
+    philox4,
+    threefry2,
+    threefry4,
+    mkPhilox2x32Gen
+    ) where
 
 import Data.Word
 import Data.Bits
@@ -8,80 +14,4 @@ import System.Random
 
 import System.Random.Random123.Philox
 import System.Random.Random123.Threefry
-
-
-class Key a where
-    fromGlobal :: IO a
-    fromSeed :: Int -> a
-
-instance Key Word32 where
-    fromGlobal = undefined
-    fromSeed = undefined
-
-
-class Counter a where
-    skip :: Int -> a -> a
-    increment :: a -> a
-    increment = skip 1
-    getWord32 :: a -> a -> Int -> (Word32, a, Int)
-    getWord64 :: a -> a -> Int -> (Word64, a, Int)
-
-
-instance Counter (Word32, Word32) where
-    skip = undefined
-    increment (c0, c1)
-        | c1 == 2^32-1 = (c0 + 1, 0)
-        | otherwise = (c0, c1 + 1)
-    getWord32 c (r0, r1) 0 = (r0, c, 1)
-    getWord32 c (r0, r1) 1 = (r1, increment c, 0)
-    getWord32 c (r0, r1) _ = error "Incorrect word counter state for getWord32"
-    getWord64 c (r0, r1) 0 = (shiftL hi 32 + lo, increment c, 0) where
-        lo = fromIntegral r1 :: Word64
-        hi = fromIntegral r0 :: Word64
-    getWord64 c (r0, r1) _ = error "Incorrect word counter state for getWord64"
-
-
-data StatefulCBRNG k c = StatefulCBRNG (k -> c -> c) k c Int
-
-
-instance (Show k, Show c) => Show (StatefulCBRNG k c) where
-    show (StatefulCBRNG biject key ctr w32_ctr) =
-        unwords ["StatefulCBRNG", show key, show ctr, show w32_ctr]
-
-
-instance (Key k, Counter c) => RandomGen (StatefulCBRNG k c) where
-
-    next (StatefulCBRNG biject key ctr w32_ctr) = (int_num, new_gen) where
-        full_random = biject key ctr
-        (int_num, new_ctr, new_w32_ctr) = if (maxBound :: Int) <= 2^31-1
-            then getInt32 (getWord32 ctr full_random w32_ctr)
-            else getInt64 (getWord64 ctr full_random w32_ctr)
-        getInt32 :: (Word32, k, c) -> (Int, k, c)
-        getInt32 (raw_num, c, wc) = ((fromIntegral raw_num) :: Int, c, wc)
-        getInt64 :: (Word64, k, c) -> (Int, k, c)
-        getInt64 (raw_num, c, wc) = ((fromIntegral raw_num) :: Int, c, wc)
-        new_gen = StatefulCBRNG biject key new_ctr new_w32_ctr
-
-    genRange _
-        | maxb <= 2^63-1 = (minb, maxb)
-        | otherwise = (-2^63, 2^63-1)
-        where
-            minb = minBound :: Int
-            maxb = maxBound :: Int
-
-    split gen = (gen, gen)
-
-
-main = do
-    let
-        key = 0 :: Word32
-        ctr = (2345 :: Word32, 124 :: Word32)
-        cbrng = StatefulCBRNG philox2 key ctr 0
-        in do
-            print $ next cbrng
-            print $ next cbrng
-            print $ next cbrng
-            print $ next cbrng
-            print $ next cbrng
-            print $ genRange cbrng
-
+import System.Random.Random123.RandomGen
---
+++ System/Random/Random123/RandomGen.hs
@@ -0,0 +1,54 @@
+module System.Random.Random123.RandomGen (
+    mkPhilox2x32Gen,
+    StatefulCBRNG32,
+    StatefulCBRNG64
+    ) where
+
+import System.Random
+import Data.Word
+
+import System.Random.Random123.Types
+import System.Random.Random123.Philox
+import System.Random.Random123.Threefry
+
+
+data StatefulCBRNG32 k c = StatefulCBRNG32 (k -> c -> c) k c Int
+data StatefulCBRNG64 k c = StatefulCBRNG64 (k -> c -> c) k c Int
+
+
+instance (Show k, Show c) => Show (StatefulCBRNG32 k c) where
+    show (StatefulCBRNG32 biject key ctr w32_ctr) =
+        unwords ["StatefulCBRNG32", show key, show ctr, show w32_ctr]
+
+
+instance (Counter c, Word32Array c) => RandomGen (StatefulCBRNG32 k c) where
+
+    next (StatefulCBRNG32 biject key ctr wctr) = (fromIntegral w32, new_gen) where
+        arr = biject key ctr
+        w32 = getWord32 wctr arr
+        (ctr', wctr') = if wctr + 1 < numWords32 arr
+            then (ctr, wctr + 1)
+            else (increment ctr, 0)
+        new_gen = StatefulCBRNG32 biject key ctr' wctr'
+
+    genRange _ = (0, min (maxBound :: Int) (2^32 - 1))
+    split gen = undefined
+
+
+instance (Counter c, Word64Array c) => RandomGen (StatefulCBRNG64 k c) where
+
+    next (StatefulCBRNG64 biject key ctr wctr) = (fromIntegral w64, new_gen) where
+        arr = biject key ctr
+        w64 = getWord64 wctr arr
+        (ctr', wctr') = if wctr + 1 < numWords64 arr
+            then (ctr, wctr + 1)
+            else (increment ctr, 0)
+        new_gen = StatefulCBRNG64 biject key ctr' wctr'
+
+    genRange _ = (0, min (maxBound :: Int) (2^64 - 1))
+    split gen = undefined
+
+
+-- | Maps an 'Integer' into a Philox2x32 generator.
+mkPhilox2x32Gen :: Integer -> StatefulCBRNG32 Word32 (Array2 Word32)
+mkPhilox2x32Gen seed = StatefulCBRNG32 philox2 (liFromInteger seed) (liFromInteger 0) 0
