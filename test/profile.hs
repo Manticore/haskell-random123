e3fafa55a35a18d97b73e0b055de119d14ed02d0
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-05-03 23:30:09 +1000

    test/profile: some cosmetic changes

--- test/profile.hs
+++ test/profile.hs
@@ -14,19 +14,23 @@ a1x64 = 123 :: Word64
 a2x64 = (123, 456) :: Array2 Word64
 a4x64 = (123, 456, 789, 101112) :: Array4 Word64
 
+iterations = 100000
+
+-- Use to profile bijections
+
 threefry2x32 = (threefry2 a2x32, a2x32)
 threefry4x32 = (threefry4 a4x32, a4x32)
 threefry2x64 = (threefry2 a2x64, a2x64)
 threefry4x64 = (threefry4 a4x64, a4x64)
-
 philox2x32 = (philox2 a1x32, a2x32)
 philox4x32 = (philox4 a2x32, a4x32)
 philox2x64 = (philox2 a1x64, a2x64)
 philox4x64 = (philox4 a2x64, a4x64)
 
-iterations = 10000
-gen = mkCBRNG32 0
-rlist = take iterations (randoms gen :: [Float])
+test_bijection test = apply_ (fst test) iterations (snd test)
+
+-- Use to profile RNGs
+test_rng = sum (take iterations (randoms (mkCBRNG32 0) :: [Float]))
 
 main :: IO ()
-main = print $ sum rlist
+main = print $ test_bijection philox2x32
