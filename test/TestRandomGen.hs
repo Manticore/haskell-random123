561add3555791fb1c7ed290b87fc3d59a6de354d
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-05-04 01:36:20 +1000

    test/TestRandomGen: added actual statistical tests

--- test/TestRandomGen.hs
+++ test/TestRandomGen.hs
@@ -12,18 +12,39 @@ import System.Random.Random123.Types
 import System.Random.Random123.RandomGen
 
 
-test_gen32 = (avg < 0.55 && avg > 0.45) @?= True where
-    gen = mkCBRNG32 123456
-    rlist = take 10000 (randoms gen :: [Float])
-    avg = sum rlist / 10000
+num_floats = 10000
+e_mean = 0.5
+e_var = 1 / 12
+e_std = sqrt e_var
+e_mean_std = e_std / (sqrt (fromIntegral num_floats))
+e_std_std = sqrt ((e_var ^^ 2) / (fromIntegral num_floats - 1) * 2)
 
-test_gen64 = (avg < 0.55 && avg > 0.45) @?= True where
+test_gen32_mean = (avg < e_mean + 5 * e_std && avg > e_mean - 5 * e_std) @?= True where
+    gen = mkCBRNG32 123456
+    rlist = take num_floats (randoms gen :: [Float])
+    avg = (sum rlist) / (fromIntegral num_floats)
+
+test_gen32_std = (std < e_std + 5 * e_std_std && std > e_std - 5 * e_std_std) @?= True where
+    gen = mkCBRNG32 123456
+    rlist = take num_floats (randoms gen :: [Float])
+    avg = (sum rlist) / (fromIntegral num_floats)
+    std = sqrt ((sum (map (\x -> (x - avg) ^^ 2) rlist)) / (fromIntegral num_floats))
+
+test_gen64_mean = (avg < e_mean + 5 * e_std && avg > e_mean - 5 * e_std) @?= True where
     gen = mkCBRNG64 123456
-    rlist = take 10000 (randoms gen :: [Float])
-    avg = sum rlist / 10000
+    rlist = take num_floats (randoms gen :: [Float])
+    avg = (sum rlist) / (fromIntegral num_floats)
+
+test_gen64_std = (std < e_std + 5 * e_std_std && std > e_std - 5 * e_std_std) @?= True where
+    gen = mkCBRNG64 123456
+    rlist = take num_floats (randoms gen :: [Float])
+    avg = (sum rlist) / (fromIntegral num_floats)
+    std = sqrt ((sum (map (\x -> (x - avg) ^^ 2) rlist)) / (fromIntegral num_floats))
 
 
 test_randomgen = testGroup "RandomGen" [
-    testCase "Default 32-bit" test_gen32,
-    testCase "Default 64-bit" test_gen64
+    testCase "Default 32-bit (mean of array of floats)" test_gen32_mean,
+    testCase "Default 32-bit (std of array of floats)" test_gen32_std,
+    testCase "Default 64-bit (mean of array of floats)" test_gen64_mean,
+    testCase "Default 64-bit (std of array of floats)" test_gen64_std
     ]
